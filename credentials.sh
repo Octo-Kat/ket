a7d266db9b79ec6cd79fd774bbeebe55ddc15803
Donald Hoskins <grommish@gmail.com>
2014-02-27 00:46:45 -0500

    Remove Creds

--- credentials.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## Credential Holder for Build Scripts
-## This allow us to mask and .gitignore RACF
-RACF="octos"
-RHOST="teamoctos.com"
-ROUT="public_html/wp-content/uploads/download-manager-files/Carrier_ROMs"
-COPY_DIR="/home/copy/shares/Octos/dubbsy/Carrier_ROMs"
-COPY_GAPPS_DIR="/home/copy/shares/Octos/dubbsy/Gapps/OctOS_GApps"
-RSYNC="http://www.teamoctos.com/wp-content/plugins/wp-filebase-pro/sync.php?cron_sync=1&key=2697d799b656b7a1ab885f62e715f3e6"
